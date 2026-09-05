3ee11db6b61ab075a0fc5fb89fe0658851574122
zr1ram <sriram.varadarajulu>
2026-09-05 12:30:56 +0530

    Changed  index.html reading and UI

---
+++ diagrams.js
@@ -0,0 +1,8 @@
+// List of relative diagram file paths.
+// Simply add any new diagram path here or drop them into your folders.
+// The viewer automatically extracts folder names as categories and file names as diagram titles.
+window.DIAGRAM_PATHS = [
+  "cloud-architecture/aws.html",
+  "cloud-architecture/azure.html",
+  "cloud-architecture/gcp.html"
+];
